4b537d8b572be6e2554fa9cbd925b0769b54bee5
aave <68873328+aa4e@users.noreply.github.com>
2025-03-07 21:44:13 +0300

    Create Enums.vb

---
+++ Enums.vb
@@ -0,0 +1,16 @@
+Namespace OWEN
+
+    Public Enum Connection As Integer
+        None
+        Eth
+        Uart
+        Usb
+    End Enum
+
+    Public Enum DeviceType As Integer
+        None
+        Trm138
+        Ukt38
+    End Enum
+
+End Namespace
